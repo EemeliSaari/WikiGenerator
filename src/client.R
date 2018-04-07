943d855e7a1b5ba2befb4eb251b5732c1b9772dd
EemeliSaari <saari.eemeli@gmail.com>
2018-04-07 20:17:47 +0300

    Finished Client class and test scripts

--- src/client.R
+++ src/client.R
@@ -2,10 +2,8 @@
 
 library(methods)
 
-#if (!exists('query_base', mode = 'function'))
 source('src/utils.R')
 
-#TODO add read user.json file for user_name and password
 
 Client <- setRefClass('Client',
     fields = list(
@@ -13,36 +11,106 @@ Client <- setRefClass('Client',
         password = 'character',
         token = 'character',
         cookies = 'list',
+        lguserid = 'numeric',
+        lgusername = 'character',
         end_point = 'character'
     ),
     methods = list(
-        login = function() {
-            token_action <- paste('?action=query&meta=tokens&type=login&format=json',  sep = '')
-            print(token_action)
-            token_response <- query_base(token_action)
+        login = function(verbose=FALSE) {
+          "Login method to login the user. Returns FALSE if the login fails, TRUE if success."
+
+            token_action <- list(action = 'query', params = list(meta = 'tokens', type = 'login', format = 'json'))
+            token_response <- do_query(token_action)
 
             # Check if the response was successful
-            if (token_response$status_code != 200)
-                return()
+            if (token_response$status == FALSE)
+                return(FALSE)
 
-            cookies <<- as.list(token_response$cookies)
-            token <<- content(token_response)$query$tokens$logintoken
+            cookies <<- as.list(token_response$body$cookies)
+            token <<- content(token_response$body)$query$tokens$logintoken
 
             login_action <- paste('?action=login', '&lgname=', user_name, '&format=json', sep = '')
             login_body <- list('lgpassword' = password, 'lgtoken' = token)
-            print(paste(end_point, login_action, sep = ''))
+
             login_response <- POST(paste(end_point, login_action, sep=''), body = login_body, set_cookies=cookies)
-            print(content(login_response))
+            login_result <- content(login_response)
+
+            # The POST will cause error if client is already logged in
+            if ('Aborted' %in% login_result$login$result)
+                logout()
+
+            if (verbose == TRUE)
+                print(login_result)
+            
+            if (login_result$login$result == "Success") {
+                return(list(status=TRUE))
+            }
+            else {
+                return(list(status=FALSE))
+            }
         },
         logout = function() {
-            #TODO
+            do_query(list(action = 'logout', params = list(format = 'json')))
         },
-        do_query = function(query) {
-            #TODO
-            return('')
+        do_query = function(query, verbose=FALSE) {
+          "Queries the end_point with query. Query must be in list format: action='', params = list(p1='',p2=''...)"
+            
+            query_body <- paste('?action=', query$action, sep='')
+            for(param in names(query$params)) {
+                value <- unlist(unname(query$params[param]))
+                query_body <- paste(query_body, '&', param, '=', value, sep='')
+            }
+            url <- paste(end_point, query_body, sep='')
+            result <- GET(url)
+
+            if (verbose == TRUE)
+                print(content(result))
+
+            out <- list(status = TRUE, body = result)
+            if (result$status_code != 200) {
+                out$status = FALSE
+                out$body = toString(result$status_code)
+            }
+            else if('error' %in% names(content(result))) {
+                out$status = FALSE
+                out$body = content(result)$error$code
+            }
+            return(out)
         }
     )
 )
 
-c <- Client(user_name = user_name, password = password, end_point=BASE_URL)
-c$login()
+
+test_login <- function() {
+  "Test a simple login scenario"
+
+    user_data <- read_user('data/user_data.json')
+    
+    client <- Client(user_name = user_data$name, password = user_data$password, end_point=BASE_URL)
+    login_result <- client$login()
+    if (login_result$status == FALSE)
+        return('LogginError')
+
+    logout_result <- client$logout()
+    if (logout_result$status == FALSE)
+        return('LogoutError')
+    
+    return('Passed')
+}
+
+
+test_query <- function() {
+  "Test the query method"
+
+    user_data <- read_user('data/user_data.json')
+    
+    query <- list(action = 'query', params = list(meta = 'tokens', type = 'login', format = 'json'))
+    client <- Client(user_name = user_data$name, password = user_data$password, end_point=BASE_URL)
+    result <- client$do_query(query)
+    if (result$status == FALSE)
+        return('Failed')
+    else
+        return('Passed')
+}
+
+test_login()
