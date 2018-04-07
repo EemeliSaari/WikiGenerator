6db9c9dd4bed581132c2a8f8aa71d919d13fae01
EemeliSaari <saari.eemeli@gmail.com>
2018-04-07 20:18:17 +0300

    Implementing fetch_ids scripts

--- src/fetch_data.R
+++ src/fetch_data.R
@@ -3,33 +3,95 @@
 library(httr)
 library(jsonlite)
 
-query_generator <- function(from, to) {
+source('src/client.R')
 
+
+fetch_ids <- function(client, id_file) {
+    
+    meta_file <- 'data/id_meta.json'
+    if (!file.exists(meta_file)) {
+        meta <- list(apfrom = '', complete = FALSE)
+    }
+    else {
+        meta <- fromJSON(readLines(meta_file))
+    }
+
+    if (!file.exists(id_file)) {
+        header <- 'ns;pageid;title'
+        write(header, id_file)
+    }
+    
+    query <- list(
+        action = 'query',
+        params = list(
+            list = 'allpages',
+            aplimit = '500',
+            filterredir = 'nonredirects',
+            apnamespace = '0',
+            apfrom = meta$apfrom,
+            format = 'json'
+        )
+    )
+    i <- 0
+    repeat {
+        write(toJSON(meta), meta_file)
+        result <- client$do_query(query)
+        meta$apfrom = content(result$body)$continue$apcontinue
+        if (content(result$body)$continue$apcontinue %in% c(NULL, '')) {
+            meta$complete = TRUE
+            write(toJSON(meta), meta_file)
+            break
+        }
+        else {
+            query$params$apfrom = meta$apfrom
+            for (page in content(result$body)$query$allpages) {
+                write(vec_to_string(page, sep=';'), id_file, append = TRUE)
+            }
+        }
+        print(content(result$body)$continue$apcontinue)
+        
+        if (i == 3)
+          break
+        
+        i = i + 1
+    }
 }
 
 
-fetch_ids <- function(base_url) {
+fetch_data <- function(client, id_file, target_folder, chunk_size = 200) {
+    print('asd')
 
-    request <- paste(base_url, action, sep = '')
-    response <- GET(request, )
-    print(str(response))
+    titles <- vec_to_string(load_titles(id_file, from=1, to=200), sep='|')
+    print(titles)
+
+    query <- list(
+        action = 'query',
+        params = list(
+            titles = titles,
+            prop = 'revisions',
+            rvprop = 'content',
+            format = 'json'
+        )
+    )
 }
 
 
-data_fetcher <- function() {
+fetcher <- function(data_paths) {
+  "Fetches the article ID's and then the articles with the ID's. Reads a data_paths variable in format list
+  [id = id_path, data = articles_path, user = user_path]."
 
-    END_POINT = 'https://fi.wikipedia.org/w/api.php'
+    user_data <- read_user(data_paths$user)
 
-    #TODO: 
-    # check if data already exists
-    # Exit if it does
-    # otherwise run:
+    client <- Client(user_name = user_data$name, password = user_data$password, end_point=BASE_URL)
+    client$login()
 
-    #LOGIN
-    fetch_ids(END_POINT)
-    #FETCH DATA WITH IDS
+    #fetch_ids(client, data_paths$id)
+    fetch_data(client, data_paths$id, data_paths$data)
+
+    client$logout()
+
+    out <- NULL
 }
 
-action = '?action=query&list=allpages&aplimit=200&apfilterredir=nonredirects&apfrom=0&apto=200&format=json'
 
-data_fetcher()
+fetcher(list(id = 'data/ids.csv', data = 'data/articles/', user = 'data/user_data.json'))
