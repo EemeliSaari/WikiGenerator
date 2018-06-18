31dd2ec4661c16d9c82e138caca9cdbc4b6961b0
EemeliSaari <saari.eemeli@gmail.com>
2018-06-18 20:49:49 +0300

    Added option parser and options

--- generator.R
+++ generator.R
@@ -1,8 +1,83 @@
 # Main script for the software
 
+options(warn = -1)
+require('optparse')
+options(warn = 0)
 
-read_args <- function() {
-    #TODO read the user commandline arguments
+for(f in list.files('src')) {
+    source(paste('src/', f, sep = ''))
+}
+
+read_args <- function(program) {
+    #Read the user commandline arguments
+
+    program_options = list(
+        generate = list(
+            #TODO
+        ),
+        train = list(
+            make_option(c('-m', '--model'), type = 'character',
+                default = FALSE, help = 'Trainable model name'            
+            ),
+            make_option(c('-v', '--verbose'), type = 'integer',
+                default = 1, help = 'Training verbose mode'       
+            ),
+            make_option(c('-ds', '--datapath'), type = 'character',
+                default = FALSE, help = 'Path for data, must contain metafile'            
+            ),
+            make_option(c('-o', '--optimizer'), type = 'character',
+                default = 'adam', help = 'Model optimizer'            
+            ),
+            make_option(c('-l', '--loss'), type = 'character',
+                default = FALSE, help = 'Loss measured'
+            ),
+            make_option(c('-u', '--hidden_units'), type = 'integer',
+                default = 4, help = 'Number of hidden units'            
+            )
+        ),
+        parse = list(
+            make_option(c('-f', '--folder'), type = 'character',
+                default = FALSE, help = 'Folder containing text files.'
+            ),
+            make_option(c('-t', '--target_folder'), type = 'character',
+                default = FALSE, help = 'Out folder for parsed files.'            
+            ),
+            make_option(c('-v', '--verbose'), type = 'integer',
+                default = 0, help = 'Verbose on parsing process.'            
+            )
+        ),
+        download = list(
+            make_option(c('-lm', '--login_method'), type = 'character',
+                default = FALSE, help = 'Login method for WikiMedia'            
+            ),
+            make_option(c('-f', '--fetch_type'), type = 'character',
+                default = FALSE, help = 'Data type to fetch ie. articles'           
+            ),
+            make_option(c('-p', '--path'), type = 'character',
+                default = 'data', help = 'Output path'
+            ),
+            make_option(c('-l', '--language'), type = 'character',
+                default = 'fi', help = 'Language specific setting'
+            ),
+            make_option(c('-u', '--user_data'), type = 'character',
+                default = FALSE, help = 'User data path'
+            )
+        )
+    )
+    handle_args(parse_args2(OptionParser(option_list = program_options[[program]])))
+}
+
+
+handle_args <- function(opts) {
+    program <- opts$args[1]
+    options <- opts$options
+    #print(options)
+    switch (program,
+        'parse' = parse_files(options$folder, options$target_folder, options$args[2:length(opts$args)], options$verbose),
+        'download' = fetcher(options$login_method, options$fetch_type, options$path, options$language, options$user_data),
+        'train' = print('reenaa'),
+        'generate' = print('generois')
+    )
 }
 
 
@@ -11,11 +86,33 @@ check_data <- function() {
 }
 
 
-check_net <- function() {
+check_model <- function() {
     #TODO load net -> train small if not found
 }
 
 
+# Programm entry point
 main <- function() {
-    #TODO main script
+    argv <- commandArgs(trailingOnly = TRUE)
+
+    arg_options <- c('generate', 'train', 'parse', 'download')
+    
+    if (length(argv) == 0) {
+        print('Provide one of the following arguments:')
+        lapply(arg_options, FUN = function(x) {
+                print(paste('-', x))
+            }
+        )
+        return(NULL)
+    }
+    if (!argv[1] %in% arg_options) {
+        print(paste('Invalid argument: ', argv[1]))
+        return(NULL)
+    }
+
+    read_args(argv[1])
+
+    return(1)
 }
+
+main()
