27c884c2d562e082e7004c38d696618bf30fa3e5
EemeliSaari <saari.eemeli@gmail.com>
2018-09-11 15:01:37 +0300

    Added basic model generator

---
+++ src/model_generator.R
@@ -0,0 +1,46 @@
+
+library(methods)
+
+
+MODELS_BASE <- 'models'
+
+
+get_available_models <- function(name) {
+    paths <- file.path(MODELS_BASE, name)
+    models <- list.files(paths)
+    for (f in models) {
+        source(file.path(paths, f))
+    }
+    return(models)
+}
+
+
+get_optimizer <- function(name, opts=list()) {
+    command <- paste('tf$train$', name, sep='')
+    optimizer <- do.call(eval(parse(text = command)), args=c(opts))
+    
+    return(optimizer)
+}
+
+
+generate_model <- function(options) {
+    "Model training process"
+
+    c(options, interface) := pop_from_list(options, "interface")
+
+    get_available_models(interface)
+
+    c(options, optimizer) := pop_from_list(options, "optimizer")
+
+    c(options, batch_size) := pop_from_list(options, "batch_size")
+    c(options, n_hidden) := pop_from_list(options, "n_hidden")
+    c(options, n_vocab) := pop_from_list(options, "n_vocab")
+
+    net <- lstm_net(batch_size, n_hidden, n_vocab)
+
+    optimizer <- get_optimizer(optimizer, options)
+
+    net <- optimizer.minimize(net)
+
+    return(net)
+}
