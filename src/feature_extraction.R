c08a59d431c322bc09463ecda49e7d149fcbca77
Eemeli Saari <saari.eemeli@gmail.com>
2018-11-01 22:31:15 +0200

    Added generator and training process

--- src/feature_extraction.R
+++ src/feature_extraction.R
@@ -47,13 +47,13 @@ text_vectorizer <- function(data) {
     len <- length(chars)
 
     dataset <- encoded
-    #dataset <- lapply(encoded, FUN=function(x){
-    #        zeros <- rep(0, len)
-    #        zeros[x] = 1
-    #        out <- zeros
-    #    }
-    #)
-    
+    dataset <- lapply(encoded, FUN=function(x){
+            zeros <- rep(0, len)
+            zeros[x] = 1
+            out <- zeros
+        }
+    )
+
     return(list(dataset, chars))
 }
 
@@ -68,31 +68,97 @@ sequencing <- function(vec, seq_length=5, step=1) {
     return(list(X, y))
 }
 
-parsed <- lapply(unlist(readLines('data/articles/926.txt')), FUN=function(x) {if(x != '') x else NULL})
-parsed <- parsed[-which(sapply(parsed, is.null))]
-
-text <- paste(unlist(parsed), collapse = '<br />')
-print(text)
-c(dataset, chars) := text_vectorizer(text)
-
-#print(chars)
-#print(dataset)
-
-batch_generator <- function(options) {
-    #c(options, )
-}
-
 opt <- list(
-    n_vocab = length(chars),
-    seq_len = 40,
+    n_vocab = 250,
+    seq_len = 30,
     n_hidden = 128,
+    batch_size = 128,
     pred_len = 1,
     interface = 'tensorflow',
     optimizer = 'AdamOptimizer'
 )
 
-batch_size = 40
+model <- lstm_1(opt$n_vocab, opt$seq_len, opt$pred_len)
 
-c(X, y) := sequencing(dataset, seq_length = opt$seq_len)
+model %>% compile(
+    loss = 'categorical_crossentropy',
+    optimizer = optimizer_adam(),
+    metrics = c('accuracy')
+)
 
+summary(model)
 
+epochs_per_file <- 10
+weights_path <- 'M:/Projects/WikiGenerator/data/weights/'
+
+generator_from_files <- function(directory) {
+    
+    files <- list.files(directory)
+    n_files <- length(files)
+    next_file <- 0
+
+    chars <- seq()
+
+    function(n = 0) {
+        next_file <<- next_file + 1
+
+        if (next_file > length(files))
+            next_file <<- 1
+
+        file <- files[next_file + n]
+
+        path <- file.path(directory, file)
+        parsed <- lapply(unlist(readLines(path)), FUN=function(x) {if(x != '') x else NULL})
+        parsed <- parsed[-which(sapply(parsed, is.null))]
+
+        text <- paste(unlist(parsed), collapse = '<br />')
+
+        c(dataset, chars) := text_vectorizer(text)
+
+        c(X, y) := sequencing(dataset, seq_length = opt$seq_len)
+
+        X_train <- array(0, dim = c(length(X), opt$seq_len, opt$n_vocab))
+        for(i in 1:length(X))
+            X_train[i, ,] = matrix(unlist(X[[i]]), nrow = opt$seq_len, ncol = opt$n_vocab)
+        y_train <- matrix(unlist(y), nrow = length(y), ncol = opt$n_vocab)
+
+        X <- c()
+        y <- c()
+        gc()
+        return(list(X_train, y_train))
+    }
+}
+
+store_metrics <- function(path, history, delimiter = ';') {
+    metrics <- history$metrics
+    
+    lines <- c()
+    if (!file.exists(path)) {
+        headers <- paste(names(metrics), collapse = ';')
+        lines = c(headers)
+    }
+    for(i in c(1:length(metrics[[1]]))) {
+        lines = c(lines, paste(unlist(lapply(names(metrics), FUN=function(x) metrics[[x]][i])), collapse = ';'))
+    }
+    lapply(lines, write, file=path, sep='\n', append=T)
+    return(NULL) 
+} 
+
+gen <- generator_from_files('data/articles/')
+training_results <- '/data/training_results.csv'
+
+train_steps <- 10
+for(step in c(1:train_steps)) {
+
+    c(X_train, y_train) := gen()
+    c(X_test, y_test) := gen(1)
+
+    history <- model %>% fit(
+        X_train, y_train, epochs=5, 
+        batch_size = opt$batch_size, verbose = 1,
+        validation_data = list(X_test, y_test)
+        #callbacks = callback_model_checkpoint(weights_path)
+    )
+    save_model_hdf5(model, file.path(weights_path, paste('weights_', toString(step), '.hdf')))
+    store_metrics(training_results, history)
+}
