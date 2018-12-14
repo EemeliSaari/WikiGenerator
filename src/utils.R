1047cf5ef470bb0dbc9b7a28a6372777a7aef101
Eemeli Saari <saari.eemeli@gmail.com>
2018-12-14 10:46:27 +0200

    Added utils for PATHS and refactorings

--- src/utils.R
+++ src/utils.R
@@ -16,9 +16,9 @@ read_user <- function(file_path) {
 
 vec_to_string <- function(data, sep=' ') {
     string <- data[1]
-    for (value in data[-1]) {
+    for (value in data[-1])
         string = paste(string, toString(value), sep=sep)
-    }
+
     return(string)
 }
 
@@ -27,9 +27,8 @@ parse_line <- function(line, sep) {
     parts <- unlist(strsplit(line, sep))
     if (length(parts) > 3) {
         last_part <- ''
-        for(part in parts[3:length(parts)]) {
+        for(part in parts[3:length(parts)])
             last_part = paste(last_part, part, sep='')
-        }
         parts = parts[1:3]
         parts[3] = last_part
     }
@@ -55,48 +54,109 @@ trim <- function(x) {
 }
 
 
-get_input_shape <- function(dataset) {
-    #TODO
-}
-
 ':=' <- function(lhs, rhs) {
     "Defines the ':='
 
     Small 'R hack' that allows you to unpack multiple output variables 
     to separate environment variables
-    
     "
     frame <- parent.frame()
     lhs <- as.list(substitute(lhs))
-    if (length(lhs) > 1) {
+    if (length(lhs) > 1)
         lhs <- lhs[-1]
-    }
+
     if (length(lhs) == 1) {
         do.call('=', list(lhs[[1]], rhs), envir=frame)
         return(invisible(NULL)) 
     }
-    if (is.function(rhs) || is(rhs, 'formula')) {
+    if (is.function(rhs) || is(rhs, 'formula'))
         rhs <- list(rhs)
-    }
-    if (length(lhs) > length(rhs)) {
+
+    if (length(lhs) > length(rhs))
         rhs <- c(rhs, rep(list(NULL), length(lhs) - length(rhs)))
-    }
-    for (i in 1:length(lhs)) {
+
+    for (i in 1:length(lhs))
         do.call('=', list(lhs[[i]], rhs[[i]]), envir=frame)
-    }
+
     return(invisible(NULL)) 
 }
 
+
 pop_from_list <- function(x, key, default=NULL) {
-    if (is.null(names(x))) {
+    if (is.null(names(x)))
         logic <- x != key
-    } 
-    else {
+    else
         logic <- names(x) != key
-    }
     value <- x[key]
-    if (is.null(value)) {
+    if (is.null(value))
         value <- default
-    }
     return(list(x[logic], unlist(unname(value))))
+}
+
+
+list_to_array <- function(data) {
+    arr <- array(0, dim = dim(data))
+}
+
+
+read_config <- function() {
+    source('config.R')
+    for(name in names(PATHS)) {
+        opt <- PATHS[[name]]
+        if(!dir.exists(opt)) {
+            print(source('Path: ', opt, ' does not exist.'))
+        }
+    }
+}
+
+
+get_file_indices <- function(path, fixture=NULL) {
+    out <- lapply(list.files(path), FUN=function(x) {
+            name <- unlist(strsplit(x, '\\.'))[1]
+            if(is.null(fixture)) {
+                parts <- unlist(strsplit(name, '_'))
+                index <- parts[length(parts)]
+            }
+            else
+                index <- unlist(strsplit(name, fixture))[2]
+            out <- index
+        }
+    )
+}
+
+
+get_latest_file_name <- function(option, fixture=NULL) {
+    path <- PATHS[[option]]
+    files <- list.files(path)
+    indices <- get_file_indices(path, fixture)
+    if(length(indices) == 0)
+        return(NULL)
+    names(indices) = files
+    latest <- names(indices[order(unlist(indices))])[1]
+    return(paste(path, latest, sep = ''))
+}
+
+
+get_path <- function(option, name = NULL) {
+    if(is.null(name))
+        path <- PATHS[[option]]
+    else
+        path <- file.path(PATHS[[option]], name)
+    if(!is.null(name) && !file.exists(path))
+        print('Given path does not exists.')
+    return(path)
+}
+
+
+store_model_weights <- function(model) {
+    path <- get_path('weights')
+    indices <- unlist(get_file_indices(path))
+    if(length(indices) == 0)
+        index <- 0
+    else
+        index <- indices[length(indices)]
+    filename <- paste('weights_', toString(index), '.h5', sep='')
+    weights_path <- file.path(path, filename)
+    save_model_hdf5(model, weights_path)
+    return(weights_path)
 }
